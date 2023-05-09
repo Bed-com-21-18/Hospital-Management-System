734d74b4c85fbe8c1999a34e8400b61060edccbc
Bed-com-21-18 <Bed-com-21-18@cc.ac.mw>
2023-05-09 11:31:07 +0200

    added current database that include drug_pice table ommited from the prvious commit

--- hms.sql
+++ hms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2023 at 04:05 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: May 09, 2023 at 11:25 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -69,7 +69,7 @@ CREATE TABLE `appointments` (
 --
 
 INSERT INTO `appointments` (`id`, `patient_id`, `date`, `time`, `professional`, `reason`, `booked_by`, `booked_at`, `status`) VALUES
-(1, 3, '2023-05-06', '18:09:00', 'Neurologist', 'Chibayo', 'Charle', '18:07:40 05-05-2023 ', 'Not Prescribed'),
+(1, 3, '2023-05-06', '18:09:00', 'Neurologist', 'Chibayo', 'Charle', '18:07:40 05-05-2023 ', 'Prescribed by chama'),
 (2, 4, '2023-05-20', '20:08:00', 'Surgeon', 'Wathyoka thako', 'Chance', '18:08:36 05-05-2023 ', 'Not Prescribed'),
 (3, 7, '2023-05-03', '18:11:00', 'Cardiologist', 'Cardiac Arrest', 'Chanco', '18:09:29 05-05-2023 ', 'Not Prescribed'),
 (4, 7, '2023-05-18', '20:37:00', 'Physiotherapist', 'Physical Intortion ', 'Charle', '18:37:38 05-05-2023 ', 'Not Prescribed'),
@@ -77,7 +77,7 @@ INSERT INTO `appointments` (`id`, `patient_id`, `date`, `time`, `professional`, 
 (6, 7, '2023-05-04', '00:35:00', 'Surgeon', 'Tione Zammimba', 'Charle Cee', '19:43:28 05-05-2023 ', 'Not Prescribed'),
 (7, 4, '2023-05-05', '21:56:00', 'Neurologist', 'ithjpto[kpferi', 'Charle', '19:55:34 05-05-2023 ', 'Prescribed by Omexie Gumba'),
 (9, 2, '2023-05-07', '09:08:00', 'Surgeon', 'Broken Leg', 'Omexie Gumba', '09:09:32 06-05-2023 ', 'Prescribed by Charle Cee'),
-(10, 5, '2023-05-06', '10:48:00', 'Surgeon', 'hfhfmhfmff', 'Charle', '10:49:27 06-05-2023 ', 'Prescribed by Charle Cee'),
+(10, 5, '2023-05-06', '10:48:00', 'Surgeon', 'hfhfmhfmff', 'Charle', '10:49:27 06-05-2023 ', 'Prescribed by chama'),
 (11, 4, '2023-05-06', '15:19:00', 'Physiotherapist', 'Urgent attention needed', 'Charle Cee', '15:17:58 06-05-2023 ', NULL),
 (12, 4, '2023-05-06', '17:29:00', 'Physiotherapist', 'Need urgent attention', 'Charle Cee', '15:30:05 06-05-2023 ', NULL),
 (13, 5, '2023-05-06', '15:33:00', 'Physiotherapist', 'Need Urgent attention', 'Charle Cee', '15:32:21 06-05-2023 ', NULL),
@@ -94,7 +94,23 @@ INSERT INTO `appointments` (`id`, `patient_id`, `date`, `time`, `professional`, 
 (24, 1, '1999-11-01', '11:11:00', 'Gastroenterologist', 'dsfdghjkl', 'Charle Cee', '21:11:53 06-05-2023 ', NULL),
 (25, 2, '2023-05-04', '23:46:00', 'Dermatologist', 'rtuyigf', 'Charle Cee', '23:44:32 06-05-2023 ', NULL),
 (26, 9, '2023-05-11', '12:22:00', 'Pediatric', 'Broken Thigh', 'Charle Cee', '00:48:23 07-05-2023 ', NULL),
-(27, 9, '2023-02-12', '12:22:00', 'Gastroenterologist', 'QHTEJRYUTIY,MNDBSVASAD X', 'Charle Cee', '16:01:43 07-05-2023 ', NULL);
+(27, 9, '2023-02-12', '12:22:00', 'Gastroenterologist', 'QHTEJRYUTIY,MNDBSVASAD X', 'Charle Cee', '16:01:43 07-05-2023 ', NULL),
+(28, 12, '2023-05-07', '20:47:00', 'Gastroenterologist', 'URGENT ATTENTION', 'matilda', '20:47:30 07-05-2023 ', NULL),
+(29, 1, '2023-05-08', '12:21:00', 'Physiotherapist', 'edrftgyhujuydtfuisru', 'ida', '16:16:12 08-05-2023 ', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bills`
+--
+
+CREATE TABLE `bills` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `bill_date` date NOT NULL,
+  `amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -115,8 +131,10 @@ CREATE TABLE `doctor` (
 
 INSERT INTO `doctor` (`id`, `uname`, `prof`, `pwd`) VALUES
 (4, 'joe viola', 'Radiologist', 'c20ad4d76fe97759aa27a0c99bff6710'),
-(5, 'jekapu', 'Physiotherapist', '81dc9bdb52d04dc20036dbd8313ed055'),
-(6, 'Charle Cee', 'Surgeon', '70965feb0441ff7fc1982fc5c509136e');
+(5, 'omexie', 'Physiotherapist', '81dc9bdb52d04dc20036dbd8313ed055'),
+(6, 'Charle Cee', 'Surgeon', '70965feb0441ff7fc1982fc5c509136e'),
+(7, 'chama', 'Surgeon', 'c20ad4d76fe97759aa27a0c99bff6710'),
+(8, 'makiga', 'Physiotherapist', 'c20ad4d76fe97759aa27a0c99bff6710');
 
 -- --------------------------------------------------------
 
@@ -126,38 +144,64 @@ INSERT INTO `doctor` (`id`, `uname`, `prof`, `pwd`) VALUES
 
 CREATE TABLE `drug` (
   `drug_name` varchar(255) NOT NULL,
-  `symptoms` varchar(255) NOT NULL
+  `symptoms` varchar(255) NOT NULL,
+  `drug_price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `drug`
 --
 
-INSERT INTO `drug` (`drug_name`, `symptoms`) VALUES
-('Paracetamol', 'Fever'),
-('Ibuprofen', 'Fever, Headache, Muscle aches'),
-('Azithromycin', 'Cough, Shortness of breath'),
-('Prednisone', 'Shortness of breath, Chest Pain'),
-('Cetirizine', 'Runny nose, Itching, Skin-rash'),
-('Ondansetron', 'Nausea, Vomiting'),
-('Loperamide', 'Diarrhoea'),
-('Diphenhydramine', 'Pink eye'),
-('Acetaminophen', 'Fever'),
-('buprofen', 'Fever'),
-('Aspirin', 'Fever'),
-('Dextromethorphan', 'Cough'),
-('Guaifenesin', 'Cough'),
-('Albuterol', 'Vomiting'),
-('Prednisone', 'Headache '),
-('Oxygen', 'Shortness of breath'),
-('Panadol', 'Loss of taste, Nausea'),
-('Buffen', 'Chest pain, Body aches'),
-('Carbamazepine', 'sore throat, skin rashes'),
-('Doxcyclin', 'Loss of Appetite Dizziness'),
-('Poaxcyclin', 'Loss of Smell, Allergy'),
-('Intoxcyclin', 'Fatigue, General Body aches,'),
-('Indosine', 'Diarrhoea, Allergy'),
-('Hedax', 'Fatigue');
+INSERT INTO `drug` (`drug_name`, `symptoms`, `drug_price`) VALUES
+('', '', 0),
+('Acetaminophen', 'Fever', 0),
+('Albuterol', 'Vomiting', 0),
+('Aspirin', 'Fever', 0),
+('Azithromycin', 'Cough, Shortness of breath', 0),
+('Buffen', 'Chest pain, Body aches', 0),
+('buprofen', 'Fever', 0),
+('Carbamazepine', 'sore throat, skin rashes', 0),
+('Cetirizine', 'Runny nose, Itching, Skin-rash', 0),
+('Dextromethorphan', 'Cough', 0),
+('Diphenhydramine', 'Pink eye', 0),
+('Doxcyclin', 'Loss of Appetite Dizziness', 0),
+('Guaifenesin', 'Cough', 0),
+('Hedax', 'Fatigue', 0),
+('Ibuprofen', 'Fever, Headache, Muscle aches', 0),
+('Indosine', 'Diarrhoea, Allergy', 0),
+('Intoxcyclin', 'Fatigue, General Body aches,', 0),
+('Loperamide', 'Diarrhoea', 0),
+('Ondansetron', 'Nausea, Vomiting', 0),
+('Oxygen', 'Shortness of breath', 0),
+('Panadol', 'Loss of taste, Nausea', 0),
+('Paracetamol', 'Fever', 0),
+('Poaxcyclin', 'Loss of Smell, Allergy', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drug_price`
+--
+
+CREATE TABLE `drug_price` (
+  `drug_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `drug_price`
+--
+
+INSERT INTO `drug_price` (`drug_name`, `price`) VALUES
+('Acetaminophen', '45.00'),
+('Albuterol', '670.00'),
+('Aspirin', '800.00'),
+('Azithromycin', '45.00'),
+('Buffen', '5455.00'),
+('Dextromethorphan', '67.00'),
+('Hedax', '789.00'),
+('Oxygen', '9999.00'),
+('Paracetamol', '67.00');
 
 -- --------------------------------------------------------
 
@@ -187,15 +231,29 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`id`, `name`, `date`, `gender`, `age`, `phoneNumber`, `district`, `village`, `residential`, `others`, `symptoms`, `history`, `prescribed_by`, `prescribed_on`) VALUES
-(1, 'Chiso Bwanali', '2458-09-09', 'female', 20, 888678728, 'Lilongwe', 'Kabudula', 'Chikanda', 'Malaria', 'Shortness of breath, Nausea, Vomiting, Dizziness', 'High Blood Pressure, Heart Disease, Lung Disease, Cancer, Liver Disease, Autoimmune Disease', 'Omexie Gumba', '10:24:53 05-05-2023 '),
+(1, 'Chiso Bwanali', '2458-09-09', 'female', 20, 888678728, 'Lilongwe', 'Kabudula', 'Chikanda', 'poiuytrew', 'Shortness of breath, Loss of taste, Diarrhoea', 'Kidney Disease', 'chama', '16:24:35 08-05-2023 '),
 (2, 'Chiso Bwanali', '1100-09-02', 'female', 17, 996348737, 'Lilongwe', 'Kachoka', 'Matawale', 'rtkdjrsgeafken', 'Fever', 'High Blood Pressure, Heart Disease, Lung Disease, Liver Disease', 'Omexie Gumba', '10:12:39 05-05-2023 '),
-(3, 'Gomboz Tech', '2020-02-02', 'male', 54, 881955791, 'Dowa', 'Kachoka', 'Kalimbuka', NULL, NULL, NULL, 'Charle Cee', '01:59:18 05-05-2023 '),
+(3, 'Gomboz Tech', '2020-02-02', 'male', 54, 881955791, 'Dowa', 'Kachoka', 'Kalimbuka', 'iiiwer', 'Shortness of breath, Diarrhoea, Itching, Chest Pain', 'High Blood Pressure', 'chama', '22:53:19 08-05-2023 '),
 (4, 'Edson Magombo', '2020-08-09', 'male', 19, 997740566, 'Dowa', 'Mponela', 'Nason', 'palibe', 'Fever, Shortness of breath, Muscle aches, Nausea, Dizziness', 'High Blood Pressure, Heart Disease, Lung Disease, Liver Disease', 'Omexie Gumba', '10:13:52 05-05-2023 '),
-(5, 'Avio', '2020-02-02', 'male', 11, 2147483647, 'Phalombe', 'olala', 'Old Naisi', 'qwqertyu', 'Cough, Vomiting', 'Diabetes, Cancer', 'Charle Cee', '23:18:24 06-05-2023 '),
+(5, 'Avio', '2020-02-02', 'male', 11, 2147483647, 'Phalombe', 'olala', 'Old Naisi', 'sneezing', 'Vomiting, Itching,  General body aches', 'High Blood Pressure, Kidney Disease', 'chama', '19:52:31 07-05-2023 '),
 (7, 'Mary Banda', '1999-04-07', 'female', 23, 882595892, 'Zomba', 'Chikanda', 'university of malawi, po box 280, zomba', NULL, NULL, NULL, NULL, NULL),
 (8, 'Maureen', '1998-03-12', 'female', 12, 882595892, 'Zomba', 'Chikanda', 'university of malawi, po box 280, zomba', NULL, NULL, NULL, NULL, NULL),
 (9, 'Zione Muliya', '1933-12-12', 'female', 90, 2147483647, 'Bangwe', 'Blantyre', 'Nyambadwe', 'Chibayo', 'Cough, Vomiting, Allergy', 'High Blood Pressure, Cancer', 'Charle Cee', '15:47:20 07-05-2023 '),
-(11, 'Charle Cee Graphix', '1988-02-21', 'Female', 35, 882595892, 'Zomba', 'Chikanda', 'university of malawi, po box 280, zomba', NULL, NULL, NULL, NULL, NULL);
+(11, 'Charle Cee Graphix', '1988-02-21', 'Female', 35, 882595892, 'Zomba', 'Chikanda', 'university of malawi, po box 280, zomba', NULL, NULL, NULL, NULL, NULL),
+(12, 'OMEXIE CHAMA', '1999-12-09', 'Female', 24, 2147483647, 'Phalombe', 'Muwake', 'Zomba', NULL, NULL, NULL, NULL, NULL),
+(13, 'mary kama', '2023-05-08', 'Female', 0, 2147483647, 'muwake', 'Muwake', 'Zomba', NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `service_name` varchar(255) NOT NULL,
+  `cost` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -215,9 +273,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `uname`, `prof`, `pwd`) VALUES
-(2, 'Avio', 'Physiotherapist', 'c4ca4238a0b923820dcc509a6f75849b'),
-(4, 'Peter', 'Radiologist', 'c4ca4238a0b923820dcc509a6f75849b'),
-(5, 'Charle Cee', 'Surgeon', '70965feb0441ff7fc1982fc5c509136e');
+(6, 'matilda', 'Receptionist', 'c20ad4d76fe97759aa27a0c99bff6710'),
+(7, 'ida', 'Clinician', 'c20ad4d76fe97759aa27a0c99bff6710');
 
 --
 -- Indexes for dumped tables
@@ -240,15 +297,41 @@ ALTER TABLE `appointments`
   ADD KEY `patient_id` (`patient_id`);
 
 --
+-- Indexes for table `bills`
+--
+ALTER TABLE `bills`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`patient_id`),
+  ADD KEY `service_id` (`service_id`);
+
+--
 -- Indexes for table `doctor`
 --
 ALTER TABLE `doctor`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `drug`
+--
+ALTER TABLE `drug`
+  ADD PRIMARY KEY (`drug_name`);
+
+--
+-- Indexes for table `drug_price`
+--
+ALTER TABLE `drug_price`
+  ADD PRIMARY KEY (`drug_name`);
+
+--
 -- Indexes for table `patient`
 --
 ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -271,25 +354,48 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `bills`
+--
+ALTER TABLE `bills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`),
+  ADD CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
